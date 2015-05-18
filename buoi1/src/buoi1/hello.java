018cfeae6308afe83064c487f83b4916abe6a83d
WIN7 <jfk1234567891@gmail.com>
2015-05-18 21:40:17 +0700

    add 1

--- buoi1/src/buoi1/hello.java
+++ buoi1/src/buoi1/hello.java
@@ -4,7 +4,8 @@ public class hello
 {
 	public static void main(String args[]) 
 	{                
-		System.out.print("I love duu! \n");        
+		System.out.print("I love duu! \n");
+		System.out.print("1 \n");
 		
 	}
 	
