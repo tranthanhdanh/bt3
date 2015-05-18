f309f63ac40da7e727db499a62ba9221c37cbdc9
WIN7 <jfk1234567891@gmail.com>
2015-05-18 21:14:57 +0700

    v1

---
+++ buoi1/src/buoi1/hello.java
@@ -0,0 +1,14 @@
+package buoi1;
+
+public class hello 
+{
+	public static void main(String args[]) 
+	{                
+		System.out.print("I love duu! \n");        
+		
+	}
+	
+} 
+
+
+
---
+++ buoi1/src/buoi1/main.java
@@ -0,0 +1,10 @@
+package buoi1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
